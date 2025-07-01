6b3bc97473aa0336cb6e438e51998af54c50696a
Maiquel Mojica <maiquel.mojica@gmail.com>
2025-06-30 21:47:00 -0400

    Add initial files for Baldatro mod including .gitignore, changelog, release script, assets, and documentation
    - Created .gitignore to exclude unnecessary files from version control.
    - Added CHANGELOG.md to document mod updates and installation instructions.
    - Implemented create_release.ps1 script for building mod release packages.
    - Included LICENSE file with MIT License terms.
    - Added manifest.json for mod metadata.
    - Created mod.lua with core mod functionality.
    - Added README.md for user guidance and support.
    - Included baldJokers.png assets in 1x and 2x resolutions.

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# Baldatro
+Baldatro is a mod that makes all the jokers bald, thats it, enjoy!
+
+
+## Updates:
+**Makes almost all Jokers bald**: Thats it, not much to say.
+
+## Installation
+To install the "Baldatro" mod, follow these steps:
+
+1. This mod requires [Steamodded](https://github.com/Steamodded/smods).
+2. Just the download the "Baldatro-vx.zip" file from releases of the mod and extract it in your C:\Users\<USER>\AppData\Roaming\Balatro\Mods or %appdata%\Balatro\Mods directory.
---
+++ mod.lua
@@ -0,0 +1,15 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Baldatro
+--- MOD_ID: Baldatro
+--- MOD_AUTHOR: [SirMaiquis]
+--- MOD_DESCRIPTION: Baldatro is a mod that makes all the jokers bald, thats it, enjoy!
+
+----------------------------------------------
+------------MOD CODE -------------------------
+G.F_NO_ACHIEVEMENTS = false
+
+sendDebugMessage("Launching Baldatro!")
+SMODS.Atlas { key = "Joker", path = "baldJokers.png", px = 71, py = 95, prefix_config = { key = false } }
+
+----------------------------------------------
+------------MOD CODE END----------------------
